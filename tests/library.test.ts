81b824131e5a0330da598cebbc31f99fdab9d054
ompanchwate2003 <ompanchwate2003@gmail.com>
2024-09-26 23:53:23 +0530

    Add test for borrow books

--- tests/library.test.ts
+++ tests/library.test.ts
@@ -8,9 +8,19 @@ describe('Library Management System', () => {
     library = new Library();
   });
 
+  // adding new book
   it('should add books to the library', () => {
     const book = new Book('1234', 'Clean Code', 'Robert C. Martin', 2008);
     library.addBook(book);
-    expect(library.getAvailableBooks().length).toBe(1); // This should fail since no code exists yet
+    expect(library.getAvailableBooks().length).toBe(1);
   });
+
+  // Borrow book
+  it('should allow a book to be borrowed if available', () => {
+    const book = new Book('1001', 'The Secret', 'Om Panchwate', 2024);
+    library.addBook(book);
+    library.borrowBook('1001');
+    expect(library.getAvailableBooks().length).toBe(0);
+  });
+  
 });
