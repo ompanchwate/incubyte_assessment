5154708c1e390d12f508ed1af1a210cf38e32cfa
ompanchwate2003 <ompanchwate2003@gmail.com>
2024-09-26 23:49:10 +0530

    Implement add book feature

--- src/models/library.ts
+++ src/models/library.ts
@@ -3,4 +3,14 @@ import { Book } from './book';
 
 export class Library {
   private books: Map<string, Book> = new Map();
+
+  // Adding new book
+  addBook(book: Book): void {
+    this.books.set(book.isbn, book);
+  }
+
+  // get available books
+  getAvailableBooks(): Book[] {
+    return Array.from(this.books.values()).filter(book => book.available);
+  }
 }
